2c65a9ca2d13ca70df2aa47c5b8fc57d34a5fd00
Alano Martins Pinto <alanomartinspinto@gmail.com>
2014-04-03 20:06:26 -0300

    MainActivity

---
+++ src/br/com/fa7/easylist/view/ItemActivity.java
@@ -0,0 +1,14 @@
+package br.com.fa7.easylist.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import br.com.fa7.easylist.R;
+
+public class ItemActivity extends Activity {
+
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_insert_item);
+	    }
+}
--- src/br/com/fa7/easylist/view/MainActivity.java
+++ src/br/com/fa7/easylist/view/MainActivity.java
@@ -1,16 +1,17 @@
-package br.com.fa7.easylist;
+package br.com.fa7.easylist.view;
 
+import br.com.fa7.easylist.R;
+import br.com.fa7.easylist.R.id;
+import br.com.fa7.easylist.R.layout;
+import br.com.fa7.easylist.R.menu;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import br.com.fa7.easylist.models.Item;
-import br.com.fa7.easylist.persistence.ItemStorage;
 
 public class MainActivity extends Activity {
 
